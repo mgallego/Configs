12f2f2f07976f0543f612824b4bc1c69adbf5765
Moises <moises@satelite.(none)>
2011-05-04 21:28:49 +0200

    first commit

---
+++ SO/auto-install.sh
@@ -0,0 +1,9 @@
+##Instalando paquetes de desarrollo
+sudo apt-get install git-core php5 php5-sqlite php5-mysql php5-cli apache2 emacs php-apc php5-intl 
+
+##creando el entorno de desarrollo
+mkdir Dev/
+
+##Cargar los repositorios de Github
++TODO
+
