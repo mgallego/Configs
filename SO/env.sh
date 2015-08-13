fa39ee201ac7ecb9c89faedb41d757a559fe61f7
Moises Gallego <moises.gallego@native.ad>
2015-08-13 17:07:37 +0200

    Install neotree, restclient, feature-mode and find-file-in-project

---
+++ SO/env.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+#Install minimun applications
+sudo apt-get install git emacs tmux zsh
+
+#ohmyzsh
+rm -rf ~/.oh-my-zsh
+wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O - | sh
+
+#Link configurations
+rm -rf ~/.zshrc ~/.tmux.conf ~/.emacs ~/.emacs.modes
+ln -s `pwd`/.zshrc ~/
+ln -s `pwd`/.tmux.conf ~/
+
+#emacs config and modes
+cd ../emacs
+rm -rf ./.emacs.modes
+./install_modes.sh
+ln -s `pwd`/.emacs ~/
+ln -s `pwd`/.emacs.modes ~/
